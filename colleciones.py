6bee45bc0b59715f3e6e1657216f7c3d8e88aa2f
LeonardoFRNG <leonardojim321@gmail.com>
2026-03-12 12:53:46 -0500

    practica de tuplas y diccionarios

---
+++ colleciones.py
@@ -0,0 +1,15 @@
+#Tuplas
+
+# tupla = (1,2,3,4,5, 'hola', True)
+
+# print(tupla.index(3))
+
+#Diccionarios
+
+usuario = {
+    "nombre": "Leonardo",
+    "apellido": "Jimenez",
+    "correo" : "leonardojim321@gmail.com"
+}
+
+print(usuario.values())
